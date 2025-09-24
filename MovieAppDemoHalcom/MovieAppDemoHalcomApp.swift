f7ef5d2d3355469967433982cdc0edc2faa0421b
vladimirOfSwift <vladimirsavic456@gmail.com>
2025-09-24 19:17:17 +0200

    Initial Commit

---
+++ MovieAppDemoHalcom/MovieAppDemoHalcomApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieAppDemoHalcomApp.swift
+//  MovieAppDemoHalcom
+//
+//  Created by Vladimir Savic on 24. 9. 2025..
+//
+
+import SwiftUI
+
+@main
+struct MovieAppDemoHalcomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
