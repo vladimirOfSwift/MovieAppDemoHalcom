cddb7a1b7d2a28351e2b729a4fedb1e5b5cf6b08
vladimirOfSwift <vladimirsavic456@gmail.com>
2025-09-25 14:41:16 +0200

    Add ProgressView while the movies are being loaded

--- MovieAppDemoHalcom/Views/MovieListView.swift
+++ MovieAppDemoHalcom/Views/MovieListView.swift
@@ -12,47 +12,70 @@ struct MovieListView: View {
     
     var body: some View {
         NavigationView {
-            List(viewModel.movies) { movie in
-                NavigationLink(destination: MovieDetailView(movie: movie)) {
-                    HStack(alignment: .top, spacing: 10) {
-                        if let posterPath = movie.posterPath, let url = URL(string: "https://image.tmdb.org/t/p/w500/\(posterPath)") {
-                            AsyncImage(url: url)
-                                .frame(width: 80, height: 120)
-                                .cornerRadius(5)
-                        } else {
-                            Color.gray.frame(width: 80, height: 120)
-                                .cornerRadius(5)
-                        }
-                        
-                        VStack(alignment: .leading, spacing: 5) {
-                            Text(movie.title)
-                                .font(.headline)
-                                .lineLimit(2)
-                            if let releaseDate = movie.releaseDate {
-                                Text("Release date: \(releaseDate)")
-                                    .font(.subheadline)
-                                    .foregroundColor(.secondary)
+            ZStack {
+                List(viewModel.movies) { movie in
+                    NavigationLink(destination: MovieDetailView(movie: movie)) {
+                        HStack(alignment: .top, spacing: 10) {
+                            if let posterPath = movie.posterPath, let url = URL(string: "https://image.tmdb.org/t/p/w500/\(posterPath)") {
+                                AsyncImage(url: url)
+                                    .frame(width: 80, height: 120)
+                                    .cornerRadius(5)
+                            } else {
+                                Color.gray.frame(width: 80, height: 120)
+                                    .cornerRadius(5)
                             }
-                            if let overview = movie.overview {
-                                Text(overview)
-                                    .font(.caption)
-                                    .foregroundColor(.secondary)
-                                    .lineLimit(3)
+                            
+                            VStack(alignment: .leading, spacing: 5) {
+                                Text(movie.title)
+                                    .font(.headline)
+                                    .lineLimit(2)
+                                if let releaseDate = movie.releaseDate {
+                                    Text("Release date: \(releaseDate)")
+                                        .font(.subheadline)
+                                        .foregroundColor(.secondary)
+                                }
+                                if let overview = movie.overview {
+                                    Text(overview)
+                                        .font(.caption)
+                                        .foregroundColor(.secondary)
+                                        .lineLimit(3)
+                                }
                             }
                         }
+                        .padding(.vertical, 8)
                     }
-                    .padding(.vertical, 8)
                 }
-               
+                .listStyle(PlainListStyle())
+                .disabled(viewModel.isLoading)
+                
+                if viewModel.isLoading {
+                    Color.black.opacity(0.25)
+                        .ignoresSafeArea()
+                    
+                    VStack(spacing: 8) {
+                        ProgressView()
+                            .progressViewStyle(CircularProgressViewStyle())
+                        Text("Loading popular movies...")
+                            .font(.subheadline)
+                    }
+                    .padding(16)
+                    .background(Color(.systemBackground))
+                    .cornerRadius(12)
+                    .shadow(radius: 10)
+                    .padding(40)
+                }
+                
+              
             }
             .navigationTitle("Popular Movies")
             .task {
                 await viewModel.fetchPopularMovies()
             }
-            .listStyle(PlainListStyle())
         }
     }
 }
+
+
 
 #Preview {
     MovieListView()
