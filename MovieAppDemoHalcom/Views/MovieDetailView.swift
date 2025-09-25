b64d33449143771a9618d0d9537b2d8e4c6efea7
vladimirOfSwift <vladimirsavic456@gmail.com>
2025-09-25 12:31:39 +0200

    Implement MovieDetailsView

---
+++ MovieAppDemoHalcom/Views/MovieDetailView.swift
@@ -0,0 +1,48 @@
+//
+//  MovieDetailView.swift
+//  MovieAppDemoHalcom
+//
+//  Created by Vladimir Savic on 25. 9. 2025..
+//
+
+import SwiftUI
+
+struct MovieDetailView: View {
+    let movie: Movie
+    
+    var body: some View {
+        ScrollView {
+            VStack(alignment: .leading, spacing: 16) {
+                if let posterPath = movie.posterPath, let url = URL(string: "https://image.tmdb.org/t/p/w500/\(posterPath)") {
+                    AsyncImage(url: url)
+                        .frame(width: 80, height: 120)
+                        .cornerRadius(5)
+                }
+                
+                Text(movie.title)
+                    .font(.title)
+                    .bold()
+                
+                if let releaseDate = movie.releaseDate {
+                    Text("Release date: \(releaseDate)")
+                        .font(.subheadline)
+                        .foregroundColor(.secondary)
+                }
+                
+                Text("Rating: \(String(format: "%.1f", movie.voteAverage))/10")
+                    .font(.subheadline)
+                    .foregroundColor(.secondary)
+                
+                Text(movie.overview ?? "No overview")
+                    .font(.body)
+            }
+            .padding()
+        }
+        .navigationTitle(movie.title)
+        .navigationBarTitleDisplayMode(.inline)
+    }
+}
+
+//#Preview {
+//    MovieDetailView()
+//}
--- MovieAppDemoHalcom/Views/MovieListView.swift
+++ MovieAppDemoHalcom/Views/MovieListView.swift
@@ -13,24 +13,34 @@ struct MovieListView: View {
     var body: some View {
         NavigationView {
             List(viewModel.movies) { movie in
-                HStack {
+                HStack(alignment: .top, spacing: 10) {
                     if let posterPath = movie.posterPath, let url = URL(string: "https://image.tmdb.org/t/p/w500/\(posterPath)") {
                         AsyncImage(url: url)
                             .frame(width: 80, height: 120)
+                            .cornerRadius(5)
                     } else {
                         Color.gray.frame(width: 80, height: 120)
+                            .cornerRadius(5)
                     }
                     
                     VStack(alignment: .leading, spacing: 5) {
                         Text(movie.title)
                             .font(.headline)
+                            .lineLimit(2)
                         if let releaseDate = movie.releaseDate {
-                            Text(releaseDate)
+                            Text("Release date: \(releaseDate)")
                                 .font(.subheadline)
                                 .foregroundColor(.secondary)
                         }
+                        if let overview = movie.overview {
+                            Text(overview)
+                                .font(.caption)
+                                .foregroundColor(.secondary)
+                                .lineLimit(3)
+                        }
                     }
                 }
+                .padding(.vertical, 5)
             }
             .navigationTitle("Popular Movies")
             .task {
