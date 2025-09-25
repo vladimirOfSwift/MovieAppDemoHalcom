60c10e483973016b9f114e1470967b8739730962
vladimirOfSwift <vladimirsavic456@gmail.com>
2025-09-25 12:49:29 +0200

    Implement MovieListView

--- MovieAppDemoHalcom/Views/MovieListView.swift
+++ MovieAppDemoHalcom/Views/MovieListView.swift
@@ -13,39 +13,43 @@ struct MovieListView: View {
     var body: some View {
         NavigationView {
             List(viewModel.movies) { movie in
-                HStack(alignment: .top, spacing: 10) {
-                    if let posterPath = movie.posterPath, let url = URL(string: "https://image.tmdb.org/t/p/w500/\(posterPath)") {
-                        AsyncImage(url: url)
-                            .frame(width: 80, height: 120)
-                            .cornerRadius(5)
-                    } else {
-                        Color.gray.frame(width: 80, height: 120)
-                            .cornerRadius(5)
-                    }
-                    
-                    VStack(alignment: .leading, spacing: 5) {
-                        Text(movie.title)
-                            .font(.headline)
-                            .lineLimit(2)
-                        if let releaseDate = movie.releaseDate {
-                            Text("Release date: \(releaseDate)")
-                                .font(.subheadline)
-                                .foregroundColor(.secondary)
+                NavigationLink(destination: MovieDetailView(movie: movie)) {
+                    HStack(alignment: .top, spacing: 10) {
+                        if let posterPath = movie.posterPath, let url = URL(string: "https://image.tmdb.org/t/p/w500/\(posterPath)") {
+                            AsyncImage(url: url)
+                                .frame(width: 80, height: 120)
+                                .cornerRadius(5)
+                        } else {
+                            Color.gray.frame(width: 80, height: 120)
+                                .cornerRadius(5)
                         }
-                        if let overview = movie.overview {
-                            Text(overview)
-                                .font(.caption)
-                                .foregroundColor(.secondary)
-                                .lineLimit(3)
+                        
+                        VStack(alignment: .leading, spacing: 5) {
+                            Text(movie.title)
+                                .font(.headline)
+                                .lineLimit(2)
+                            if let releaseDate = movie.releaseDate {
+                                Text("Release date: \(releaseDate)")
+                                    .font(.subheadline)
+                                    .foregroundColor(.secondary)
+                            }
+                            if let overview = movie.overview {
+                                Text(overview)
+                                    .font(.caption)
+                                    .foregroundColor(.secondary)
+                                    .lineLimit(3)
+                            }
                         }
                     }
+                    .padding(.vertical, 8)
                 }
-                .padding(.vertical, 5)
+               
             }
             .navigationTitle("Popular Movies")
             .task {
                 await viewModel.fetchPopularMovies()
             }
+            .listStyle(PlainListStyle())
         }
     }
 }
